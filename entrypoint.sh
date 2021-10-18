e6ecd9e0c75f1cf8d79eee069feec778058ab0f2
aseemchopra25 <aseemchopra@protonmail.com>
2021-10-18 08:17:14 +0530

    final commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@
 # $1 is the directory containing the source code
 # $2 is the directory where autodoc should be executed
 
-# By default ./src is the directory containing the clojure/script source code
+# By default ./src is the directory containing the clojure source code
 # By default . is the directory where autodoc is executed 
 
 # Testing presence of path ($2)
@@ -27,13 +27,17 @@ fi
 
 # Updating system 
 apk update 
-# Download Ca Certificated 
+# Download Ca Certificates
 apk add ca-certificates
 update-ca-certificates 
+# Installing Java
 apk add openjdk8-jre
+# Downloading AutoDoc (https://github.com/tomfaulhaber/autodoc)
 wget -O autodoc.jar https://github.com/downloads/tomfaulhaber/autodoc/autodoc-0.9.0-standalone.jar
+# Checking SHA256 and echoing it again 
 sha256sum autodoc.jar 
 echo "6035c1e1c142e6c24b23ecc107168f47dc068bc973e4fc1cf30be489617a22e0"
+# Generating Clojure Documentation
 java -jar autodoc.jar --source-path $1
-
+# Exiting
 exit 0
